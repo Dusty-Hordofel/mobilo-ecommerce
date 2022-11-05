30530d8b3e742de0869966d98f786af03c7cdb47
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-11-05 18:30:50 +0100

    41. Filters - Price

--- client/src/context/filter_context.js
+++ client/src/context/filter_context.js
@@ -80,6 +80,10 @@ export const FilterProvider = ({ children }) => {
       value = e.target.dataset.color;
     }
 
+    if (name === "price") {
+      value = Number(value);
+    }
+
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
   };
 
