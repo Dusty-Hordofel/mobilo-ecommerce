445206483fa177f1a0f93a0b2cfebe9adaa71315
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-11-06 16:55:04 +0100

    56. Cart - Calculate Totals

--- client/src/components/CartButtons.js
+++ client/src/components/CartButtons.js
@@ -8,23 +8,34 @@ import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
+  const { total_items, clearCart } = useCartContext();
+  const myUser = true;
+
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-
-      <button type="button" className="auth-btn">
-        Logout <FaUserMinus />
-      </button>
-
-      <button type="button" className="auth-btn">
-        Login <FaUserPlus />
-      </button>
+      {myUser ? (
+        <button
+          type="button"
+          className="auth-btn"
+          onClick={() => {
+            clearCart();
+            localStorage.removeItem("user");
+          }}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type="button" className="auth-btn">
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   );
 };
