bedbdaf68a2836a2280fd4caeaec59b5025f2ed3
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2022-11-06 12:45:28 +0100

    43. Filter Functionality

--- client/src/reducers/filter_reducer.js
+++ client/src/reducers/filter_reducer.js
@@ -21,18 +21,15 @@ const filter_reducer = (state, action) => {
       filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
-
   if (action.type === SET_GRIDVIEW) {
     return { ...state, grid_view: true };
   }
   if (action.type === SET_LISTVIEW) {
     return { ...state, grid_view: false };
   }
-
   if (action.type === UPDATE_SORT) {
     return { ...state, sort: action.payload };
   }
-
   if (action.type === SORT_PRODUCTS) {
     const { sort, filtered_products } = state;
     let tempProducts = [...filtered_products];
@@ -63,16 +60,52 @@ const filter_reducer = (state, action) => {
     }
     return { ...state, filtered_products: tempProducts };
   }
-
   if (action.type === UPDATE_FILTERS) {
     const { name, value } = action.payload;
     return { ...state, filters: { ...state.filters, [name]: value } };
   }
-
   if (action.type === FILTER_PRODUCTS) {
-    console.log("Filtering products");
-  }
+    const { all_products } = state;
+    const { text, category, company, color, price, shipping } = state.filters;
 
+    let tempProducts = [...all_products];
+    // filtering
+    // text
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
+    }
+    // category
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+
+    // company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+    // colors
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+    // price
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+    // shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter(
+        (product) => product.shipping === true
+      );
+    }
+
+    return { ...state, filtered_products: tempProducts };
+  }
   if (action.type === CLEAR_FILTERS) {
     return {
       ...state,
@@ -87,7 +120,6 @@ const filter_reducer = (state, action) => {
       },
     };
   }
-  return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
